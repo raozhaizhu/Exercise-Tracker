bbb7526c604c10e98b62d0da69417e7aa9706214
raozhaizhu <raozhaizhu@gmail.com>
2025-01-31 12:42:07 +0800

    fix

--- index.js
+++ index.js
@@ -25,6 +25,12 @@ app.post('/api/users', (req, res) => {
     res.json({ username, _id });
 });
 
+// 从api/users可以get到一个数组, 里面的元素都是对象,包含对应的username和_id
+app.get('/api/users', (req, res) => {
+    const userList = Object.values(users);
+    res.json(userList);
+});
+
 app.post('/api/users/:_id/exercises', (req, res) => {
     const { _id } = req.params;
     const { description, duration, date } = req.body;
@@ -53,8 +59,27 @@ app.post('/api/users/:_id/exercises', (req, res) => {
     // 返回数据
     res.json({ username, count: logs[_id].length, _id, log: logs[_id] });
 });
+//从/api/users/:_id/logs可以GET到完整的用户的训练记录
+app.get('/api/users/:_id/logs', (req, res) => {
+    const { _id } = req.params;
+    const { username } = users[_id];
+    const log = logs[_id] || [];
+    const { from, to, limit } = req.query;
+    if (from) {
+        log = log.filter((exercise) => new Date(exercise.date) >= new Date(from));
+    }
+    if (to) {
+        log = log.filter((exercise) => new Date(exercise.date) <= new Date(to));
+    }
+    if (limit) {
+        log = log.slice(0, limit);
+    }
+    res.json({ username, count: log.length, _id, log });
+});
 
 const listener = app.listen(process.env.PORT || 3000, () => {
     console.log('Your app is listening on port ' + listener.address().port);
 });
+
+//{"username":"rzz","count":1,"_id":"h7Nq9H24m","log":[{"description":"33","duration":"44","date":"2025-01-05T00:00:00.000Z"}]}
 
